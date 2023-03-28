20d5b08c2a1b23fe7375e9ed875fe50ffac4950a
Supzero-0 <mat.lecanu@gmail.com>
2023-03-28 16:09:27 +0200

    complete quest

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,24 @@
 function PokemonCard() {
+
+    const pokemonList = [
+        {
+            name: "bulbasaur",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+            name: "mew",
+        },
+    ];
+
+    const pokemon = pokemonList[1];
+
+
     return <div>
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.name === "bulbasaur" ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-
     </div>;
 }
 
