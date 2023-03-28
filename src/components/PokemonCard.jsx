f5f968fa9b806281ccf595fe05cdc3ea07a5e7c9
Supzero-0 <mat.lecanu@gmail.com>
2023-03-28 16:36:23 +0200

    complete quest

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({ pokemon }) {
 
     return <div>
@@ -6,6 +8,13 @@ function PokemonCard({ pokemon }) {
             <figcaption>{pokemon.name}</figcaption>
         </figure>
     </div>;
+}
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    })
 }
 
 export default PokemonCard;
